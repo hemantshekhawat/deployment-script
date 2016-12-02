3f504b4c24e02d16fdc718f53c9138795c26f1f6
Hemant Shekhawat <hemantshekhawat@users.noreply.github.com>
2016-12-02 13:00:26 +0530

    Update execute-deplyment.sh

--- execute-deplyment.sh
+++ execute-deplyment.sh
@@ -2,26 +2,26 @@
 
 
 printf "\n\n Deploying Sellfie Admin - Dev Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0 sellfie-admin dev
+sh deployment-script/deploy.sh dev TagV1.0.0 sellfie-admin dev
 printf "\n\n Deploying Sellfie Admin - QA Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0 sellfie-admin qa
+sh deployment-script/deploy.sh dev TagV1.0.0 sellfie-admin qa
 
 printf "\n\n Deploying Sellfie API base - Dev Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0  sellfy dev
+sh deployment-script/deploy.sh dev TagV1.0.0  sellfy dev
 printf "\n\n Deploying Sellfie API base - QA Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0 sellfy qa
+sh depdeployment-script/deploy.shh dev TagV1.0.0 sellfy qa
 
 
 
 printf "\n\n Deploying Sellfie Buyer App(Node) - Dev Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0 sellfie-front-end dev sellfie-buyer
+sh deployment-script/deploy.sh dev TagV1.0.0 sellfie-front-end dev sellfie-buyer
 printf "\n\n Deploying Sellfie Buyer App(Node)  - QA Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0 sellfie-front-end qa sellfie-buyer
+sh deployment-script/deploy.sh dev TagV1.0.0 sellfie-front-end qa sellfie-buyer
 
 printf "\n\n Deploying Sellfie Web App(Node) - Dev Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0 sellfie-front-end dev sellfie-webapp
+sh deployment-script/deploy.sh dev TagV1.0.0 sellfie-front-end dev sellfie-webapp
 printf "\n\n Deploying Sellfie Web App(Node) - QA Server with TagV1.0 \n\n"
-sh deploy-my-website.sh dev TagV1.0 sellfie-front-end qa sellfie-webapp
+sh deployment-script/deploy.sh dev TagV1.0.0 sellfie-front-end qa sellfie-webapp
 
 
 
